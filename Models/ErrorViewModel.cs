a7dc24c750783aebc8f176ef056a23e28fb13152
huyhoang084 <zkaka301@gmail.com>
2025-10-15 23:10:15 +0700

    Delete Models directory

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Doan_TranTheAnh_2020607026.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
